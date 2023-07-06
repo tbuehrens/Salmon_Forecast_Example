8bb42d36d2516ce251fa232c161c4d82b9bd4b39
casruff <cruff@skagitcoop.org>
2023-07-06 12:05:24 -0700

    updated SARIMA models fit to catch and trap counts of Baker River sockeye through 7/5/2023

--- functions/get_best_model.r
+++ functions/get_best_model.r
@@ -1,7 +1,11 @@
 get_best_model<-function(forecasts,ensembles,stack_metric,forecast_skill){
+   
   best_model<-forecasts%>%
-    bind_rows(ensembles)%>%
-    mutate(sd=abs(log(U95)-log(Estimate))/1.96)%>%
-    left_join(forecast_skill)%>%
-    filter(get(stack_metric)==min(get(stack_metric)))
+     bind_rows(ensembles)%>%
+     mutate(sd=abs(log(U95)-log(Estimate))/1.96)%>%
+     left_join(forecast_skill)%>%
+     filter(Model %in% unique(ensembles$Model))%>%
+     filter(get(stack_metric)==min(get(stack_metric)))
+
+   
 }
